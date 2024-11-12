868d5f83c735db8f4453e3914491d343fbd25a35
Samuel Khuvis <skhuvis@osc.edu>
2024-11-12 11:26:09 -0800

    Fix typo in model arg

--- test_ct.py
+++ test_ct.py
@@ -126,7 +126,7 @@ def generate_submission(model, device, site):
     envVariables.append({'key': 'CT_CONTROLLER_NODE_TYPE', 'value': device})
     envVariables.append({'key': 'CT_CONTROLLER_GPU', 'value': enable_gpu(device)})
     envVariables.append({'key': 'CT_CONTROLLER_CONFIG_PATH', 'value': '/config.yml'})
-    envVariables.append({'key': 'CT_CONTROLLER_MODEL', 'value', model})
+    envVariables.append({'key': 'CT_CONTROLLER_MODEL', 'value': model})
     d['parameterSet'] = {'envVariables': envVariables}
     d['archiveFilter'] = {'includeLaunchFiles': False}
     return d
