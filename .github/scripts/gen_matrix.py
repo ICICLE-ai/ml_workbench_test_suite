502babc4978f42b331112d10cff4cc731bd4974b
Samuel Khuvis <skhuvis@osc.edu>
2026-09-03 08:45:10 -0700

    Fix bugs in matrix generation

--- .github/scripts/gen_matrix.py
+++ .github/scripts/gen_matrix.py
@@ -12,7 +12,7 @@ for exp, full_id in zip(get_all_experiments(), get_all_experiment_ids()):
     model, device, site, dataset, mode = exp
     dataset_name = dataset["name"] if isinstance(dataset, dict) else str(dataset)
 
-    k_id = full_id.split('@')][1:]
+    k_id = full_id.split('@')[-1]
 
     matrix.append({
         "full_id": full_id,
